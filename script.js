64f06a5e4f9bdbac1a1122d41d00b3fab8bf7769
thm.dev <jeffinho850@gmail.com>
2023-06-20 21:30:26 -0300

    feat:portfolio responsivo pronto

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-function clickMenu() {
-    if (menu.style.display == 'none') {
-        menu.style.display = 'block'
-        menu.style.margin = '30px'
-        itens.style.display = 'grid'
-        nav.style.position = 'relative'
-        nav.style.height = '200px'
-        img.style.height = '100px'
-    }
-    else{
-        menu.style.display = 'none'
-        nav.style.height = '100px'
-        inicio.style.margin = '0'
-        menu.style.margin = '0'
-    }
-}
+// function clickMenu() {
+//     if (menu.style.display == 'none') {
+//         menu.style.display = 'block'
+//         menu.style.margin = '30px'
+//         itens.style.display = 'grid'
+//         nav.style.position = 'relative'
+//         nav.style.height = '200px'
+//         img.style.height = '100px'
+//     }
+//     else{
+//         itens.style.display = 'flex'
+//         menu.style.display = 'none'
+//         nav.style.height = '100px'
+//         inicio.style.margin = '0'
+//         menu.style.margin = '0'
+//     }
+// }
